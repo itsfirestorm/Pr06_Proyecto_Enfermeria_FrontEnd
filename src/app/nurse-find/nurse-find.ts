a0031bf7390362b760506b665509d65f426e0c2f
aniol <aniolnicolauroig@gmail.com>
2025-11-21 15:30:24 +0100

    create component nurse-find

---
+++ src/app/nurse-find/nurse-find.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nurse-find',
+  imports: [],
+  templateUrl: './nurse-find.html',
+  styleUrl: './nurse-find.css',
+})
+export class NurseFind {
+
+}
